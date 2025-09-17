990d19dc29de1d68e4cc1355d2130da18dd91dc4
octomonoli1 <octavio.mo90@gmail.com>
2025-09-17 12:41:05 +0200

    resolvemos ejercicios 3 y 4

--- ud1_introduc_js/script.js
+++ ud1_introduc_js/script.js
@@ -1,7 +1,4 @@
-function pintar_ruta(){
-    console.log("El acceso a la ruta C:\\\\usuario\\tarda 1'23\", algo considerado \"lento\" en la actualidad.");
-}
-
+//Ejercicio 1
 function saludar(){
     var edad = window.prompt("Introduce tu edad");
     if(edad >= 18){
@@ -10,3 +7,22 @@ function saludar(){
         console.log("Eres menor de edad");
     }
 }
+
+//Ejercicio 2
+function pintar_ruta(){
+    console.log("El acceso a la ruta C:\\\\usuario\\tarda 1'23\", algo considerado \"lento\" en la actualidad.");
+}
+
+//Ejercicio 3
+function pintar_ruta_v2(){
+    var substrng1 = "El acceso a la ruta C:\\\\usuario\\tarda 1'23\", algo";
+    var substrng2 = " considerado \"lento\" en la actualidad.";
+    console.log(substrng1 + substrng2);
+}
+
+//Ejercicio 4: Almacena y muestra por consola una variable que almacene el valor de 2*10^-9
+function muestra_potencia(){
+    var num = 2*Math.pow(10,-9);
+    console.log("El valor de 2*10^-9 es: " + num);
+}
+
