331a17bf3e9860f2289acf26a64334931fbc5667
vricheb973 <vricheb973@g.educaand.es>
2025-10-21 12:50:35 +0200

    finalizado ej1_bol1

--- ud2_typescript/boletin1/script.js
+++ ud2_typescript/boletin1/script.js
@@ -1,15 +1,12 @@
 //Ejercicio 1
-function ej1(): void{
-
+function ej1() {
     //No existe date primitivo
-    let fecha: Date = new Date();
-
+    var fecha = new Date();
     console.log("Hoy es: " + fecha.getDate() + "/" + (fecha.getMonth() + 1) + "/" + fecha.getFullYear());
     console.log("Son las: " + fecha.getHours() + ":" + fecha.getMinutes() + ":" + fecha.getSeconds());
     console.log("Hora aaaaaaa: " + (fecha.getHours() + 1) + ":" + fecha.getMinutes() + ":" + fecha.getSeconds());
     console.log("Hora antertior: " + (fecha.getHours() - 1) + ":" + fecha.getMinutes() + ":" + fecha.getSeconds());
 }
-
 //Ejercicio 2: Función que valida un dato introducido por formulario para que cumpla con un patrón de email.
 /** Los pasos para validar esto son:
              *  1-. Extraer la información desde el Input HTML
@@ -19,38 +16,32 @@ function ej1(): void{
              *  5-. Vamos a pintar el mensaje aplicando un estilo (rojo -> error \ verde -> ok)
              *  6-. Como extra, vamos a hacer que el mensaje desaparezca automáticamente a los 5 segundos
          */
-function valida_form(){    
-    const regExp = new RegExp("[^\s@]+@[^\s@]+\.[^\s@]+$"); //Con fallos (hay que corregir)
-    
+function valida_form() {
+    var regExp = new RegExp("[^\s@]+@[^\s@]+\.[^\s@]+$"); //Con fallos (hay que corregir)
     //Aplico la validacion de la expresion regular
-    if(regExp.test($inputValue("email"))){
+    if (regExp.test($inputValue("email"))) {
         //Si test devuelve true quiere decir que el texto "email" cumple la expresion reg.
-        $writeNode("ok","El email es valido") //Aquí escribo
-        $writeNode("error",""); //Aquí escribo tambien (lo que pasa que borra)
-    }else{
+        $writeNode("ok", "El email es valido"); //Aquí escribo
+        $writeNode("error", ""); //Aquí escribo tambien (lo que pasa que borra)
+    }
+    else {
         //Si es false quiere decir que el texto "email" no cumple la expresion regular.
-        $writeNode("ok","") //Aquí escribo (borrando el contenido)
-        $writeNode("error", "El email no cumple la expresion") //Aqui tambien escribo
+        $writeNode("ok", ""); //Aquí escribo (borrando el contenido)
+        $writeNode("error", "El email no cumple la expresion"); //Aqui tambien escribo
     }
 }
-
 //Helpers (comunes para todo el boletín)
-function $inputValue(id: string) : string{
-    const input = document.getElementById(id) as HTMLInputElement; //Lectura
-    let result = "";
-
-    if(input){
+function $inputValue(id) {
+    var input = document.getElementById(id); //Lectura
+    var result = "";
+    if (input) {
         result = input.value; //Recojo el valor (aqui es donde leo)
     }
-
     return result;
 }
-
-function $writeNode (id: string, msg: string) : void {
-    const nodo = document.getElementById(id) as HTMLElement; //Escritura
-
-    if(nodo){
+function $writeNode(id, msg) {
+    var nodo = document.getElementById(id); //Escritura
+    if (nodo) {
         nodo.textContent = msg;
     }
-
 }
