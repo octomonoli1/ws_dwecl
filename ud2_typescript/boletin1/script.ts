d9b7bf1bafc9cfbce6b27aa2236a17ffc8dbab00
octomonoli1 <octavio.mo90@gmail.com>
2025-10-21 11:49:32 +0200

    cambios en el ej1

--- ud2_typescript/boletin1/script.ts
+++ ud2_typescript/boletin1/script.ts
@@ -10,20 +10,41 @@ function ej1(): void{
     console.log("Hora antertior: " + (fecha.getHours() - 1) + ":" + fecha.getMinutes() + ":" + fecha.getSeconds());
 }
 
-//Ejercicio 2
-function valida_form(){
-    //let expReg: RegExp = /lo_que_sea/
-    let expReg: RegExp = new RegExp("lo_que_sea");
-
-    //Primero nos aseguramos que no es nulo (porque no puedo almacenar un nulo en un HTMLInputElement)
-    if(document.getElementById("email")){
-        let mensajeOK = document.createElement('span') ;
-        let email: HTMLInputElement  = document.getElementById("email") as HTMLInputElement;
-        if(expReg.test(email.value)){
-            mensajeOK.textContent = "Email correcto";
-        }else{
-            mensajeOK.textContent = "Email incorrecto";
-        }
+//Ejercicio 2: Función que valida un dato introducido por formulario para que cumpla con un patrón de email.
+/** Los pasos para validar esto son:
+             *  1-. Extraer la información desde el Input HTML
+             *  2-. Crear la expresión regular para validar el dato recogido (Deslgosaremos parte por parte dicha expresión)
+             *  3-. Controlar el resultado de la validación para informar al usuario
+             *  4-. Definir un espacio "visible" dentro del formulario donde pintar la información
+             *  5-. Vamos a pintar el mensaje aplicando un estilo (rojo -> error \ verde -> ok)
+             *  6-. Como extra, vamos a hacer que el mensaje desaparezca automáticamente a los 5 segundos
+         */
+function valida_form(){    
+    const regExp = new RegExp("blablabla");
+    if(regExp.test($inputValue("email"))){
+        $writeNode("ok","El email introducido es válido");
+    }else{
+        $writeNode("error","El email introducido no cumple la validación");
     }
+}
+
+//Helpers (comunes para todo el boletín)
+function $inputValue(id: string) : string{
+    const input = document.getElementById(id) as HTMLInputElement;
+    var result = "";
     
+    if(input){
+        result = input.value;
+    }
+
+    return result;
+}
+
+function $writeNode(id: string, msg: string) : void{
+    const node = document.getElementById(id) as HTMLElement;
+
+    if(node){
+        node.textContent = msg;
+    }
+
 }
