3f6f4343154f2db47430f4f2eb38a928b57e515d
vricheb973 <vricheb973@g.educaand.es>
2025-10-30 13:47:37 +0100

    boletin3

--- ud2_typescript/boletin2/script.js
+++ ud2_typescript/boletin2/script.js
@@ -68,3 +68,80 @@ function analiza_edad() {
     lista.appendChild(listaDivisores);
     lista.appendChild(edadTexto);
 }
+function cambiar_nombre() {
+    var name = prompt("Introduce tu nombre: ");
+    var saludo = document.createElement("p");
+    document.body.appendChild(saludo);
+    saludo.textContent = "Hola, " + name + "!";
+}
+function cambiar_color() {
+    var select = document.getElementById("color");
+    var colorOption = select.options[select.selectedIndex];
+    var color = colorOption.value;
+    var saludo = document.getElementById("saludo");
+    saludo.style.color = color;
+}
+function info_navegador() {
+    var input = document.getElementById("option");
+    var option = input.value;
+    switch (option) {
+        case "a":
+            idioma_navegador();
+            break;
+        case "b":
+            nombre_navegador();
+            break;
+        case "c":
+            cookies_navegador();
+            break;
+        case "d":
+            tam_navegador();
+            break;
+        default:
+            console.error("La opción introducida no es correcta");
+    }
+}
+function idioma_navegador() {
+    console.log("Idioma " + window.navigator.language);
+}
+function nombre_navegador() {
+    var userAgent = window.navigator.userAgent;
+    var navegador = "Desconocido";
+    if (userAgent.includes("Chrome") && !userAgent.includes("Edg") && !userAgent.includes("OPR")) {
+        navegador = "Chrome";
+    }
+    else if (userAgent.includes("Safari") && !userAgent.includes("Chrome")) {
+        navegador = "Safari";
+    }
+    else if (userAgent.includes("Firefox")) {
+        navegador = "Firefox";
+    }
+    else if (userAgent.includes("Edg")) {
+        navegador = "Edge";
+    }
+    else if (userAgent.includes("OPR") || userAgent.includes("Opera")) {
+        navegador = "Opera";
+    }
+    console.log("Nombre navegador: " + navegador);
+}
+function cookies_navegador() {
+    console.log("El navegador " + (window.navigator.cookieEnabled ? "si" : "no") + " tiene habilitadas las cookies");
+}
+function tam_navegador() {
+    console.log("El navegador mide " + window.innerWidth + " x " + window.innerHeight);
+}
+function actualizar_reloj() {
+    var now = new Date();
+    var hora = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+    var reloj = document.getElementById("reloj");
+    reloj.textContent = hora;
+}
+window.onload = function () { return setInterval(actualizar_reloj, 1000); };
+function navegar() {
+    var inputUrl = document.getElementById("url");
+    var url = inputUrl.value;
+    if (!url.includes("https://")) {
+        url = "https:\\" + url;
+    }
+    window.location.href = url;
+}
--- ud2_typescript/boletin2/script.ts
+++ ud2_typescript/boletin2/script.ts
@@ -82,3 +82,95 @@ function analiza_edad(){
     lista.appendChild(listaDivisores);
     lista.appendChild(edadTexto);
 }
+
+function cambiar_nombre(){
+    let name: string = prompt("Introduce tu nombre: ") as string;
+    let saludo: HTMLParagraphElement = document.createElement("p") as HTMLParagraphElement;
+    document.body.appendChild(saludo
+
+    )
+    saludo.textContent = "Hola, " + name + "!";
+}
+
+function cambiar_color(){
+    let select: HTMLSelectElement = document.getElementById("color") as HTMLSelectElement;
+    let colorOption: HTMLOptionElement = select.options[select.selectedIndex] as HTMLOptionElement;
+    let color = colorOption.value;
+
+    let saludo: HTMLParagraphElement = document.getElementById("saludo") as HTMLParagraphElement;
+    saludo.style.color = color;
+}
+
+function info_navegador(){
+    let input: HTMLInputElement = document.getElementById("option") as HTMLInputElement;
+    let option : string = input.value;
+
+    switch(option){
+        case "a":
+            idioma_navegador();
+            break;
+        case "b":
+            nombre_navegador();
+            break;
+        case "c": 
+            cookies_navegador();
+            break;
+        case "d":
+            tam_navegador();
+            break;
+        default:
+            console.error("La opción introducida no es correcta");
+    }
+}
+
+function idioma_navegador(): void {
+    console.log("Idioma " + window.navigator.language);
+}
+
+function nombre_navegador() : void {
+
+    let userAgent: string = window.navigator.userAgent as string;
+    let navegador = "Desconocido";
+
+    if (userAgent.includes("Chrome") && !userAgent.includes("Edg") && !userAgent.includes("OPR")) {
+        navegador = "Chrome";
+    } else if (userAgent.includes("Safari") && !userAgent.includes("Chrome")) {
+        navegador = "Safari";
+    } else if (userAgent.includes("Firefox")) {
+        navegador = "Firefox";
+    } else if (userAgent.includes("Edg")) {
+        navegador = "Edge";
+    } else if (userAgent.includes("OPR") || userAgent.includes("Opera")) {
+        navegador = "Opera";
+    }
+
+    console.log("Nombre navegador: " + navegador);
+}
+
+function cookies_navegador(): void {
+    console.log("El navegador " + (window.navigator.cookieEnabled? "si" :  "no") + " tiene habilitadas las cookies");
+}
+
+function tam_navegador(): void {
+    console.log("El navegador mide " + window.innerWidth + " x " + window.innerHeight);
+}
+
+function actualizar_reloj(): void{
+    let now = new Date();
+    let hora = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+    let reloj: HTMLParagraphElement = document.getElementById("reloj") as HTMLParagraphElement;
+    reloj.textContent = hora;
+}
+
+window.onload = () => setInterval(actualizar_reloj, 1000);
+
+function navegar(){
+    let inputUrl: HTMLInputElement = document.getElementById("url") as HTMLInputElement;
+    let url: string = inputUrl.value;
+
+    if(!url.includes("https://")){
+        url = "https:\\" + url;
+    }
+
+    window.location.href = url;
+}
